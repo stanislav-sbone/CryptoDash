9de2345f06335875f22340c8ef3aec35d3eed743
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-12-12 21:57:04 +0300

    fix(coin-details): improve responsive styles for CoinHeader and remove PriceChange tag for mobile devices

--- src/components/CoinDetails/CoinHeader.tsx
+++ src/components/CoinDetails/CoinHeader.tsx
@@ -19,19 +19,19 @@ const CoinHeader: FC<IProps> = ({
   const isLowerThanZero = price_change_percentage_24h <= 0;
 
   return (
-    <div className="flex items-center justify-between md:justify-normal md:gap-2 py-4 lg:px-4">
+    <div className="flex items-center gap-2 py-4 lg:px-4">
       <div className="px-2 text-[18px] dark:text-[#a1a7bb] border border-[#a1a7bb] rounded-xl bg-gray-600">
         {`#${market_cap_rank}`}
       </div>
-      <img src={image} alt={symbol} />
-      <h2 className="text-[24px] tracking-wider text-black dark:text-gray-300 font-medium">
+      <img src={image} alt={symbol} className="w-10" />
+      <h2 className="text-[20px] md:text-[24px] tracking-wider text-black dark:text-gray-300 font-medium">
         {name}{' '}
         <span className="text-gray-700 dark:text-gray-400 tracking-tight">
           {symbol.toUpperCase()}
         </span>
       </h2>
       <div
-        className={`px-2 text-[16px] ${isLowerThanZero ? 'text-red-200 dark:text-red-900 bg-red-500' : 'text-green-900 bg-green-500'} border border-[#a1a7bb] rounded-xl`}
+        className={`hidden md:block px-2 text-[16px] ${isLowerThanZero ? 'text-red-200 dark:text-red-900 bg-red-500' : 'text-green-900 bg-green-500'} border border-[#a1a7bb] rounded-xl`}
       >
         {isLowerThanZero ? <CaretDownOutlined /> : <CaretUpOutlined />}
         {`${price_change_percentage_24h.toFixed(2)}%`}

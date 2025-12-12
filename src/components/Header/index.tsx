47512c1f5f5c9c9fce99dd8092705872ed813d62
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-12-12 22:16:43 +0300

    feat(header): add sticky positioning and border bottom on scroll

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, type FC } from 'react';
+import { useEffect, useRef, useState, type FC } from 'react';
 import Logo from './Logo';
 import LanguageDropdown from './LanguageDropdown';
 import ThemeButton from './ThemeButton';
@@ -6,10 +6,26 @@ import BurgerMenu from './BurgerMenu';
 
 const Header: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
   const menuToggleRef = useRef<HTMLButtonElement | null>(null);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="flex justify-between items-center relative px-6 2xl:px-24 py-4 sm:py-6 mb-1 sm:mb-2">
+    <header
+      className={`sticky top-0 z-50 flex justify-between items-center px-6 2xl:px-24 py-4 sm:py-6 mb-1 sm:mb-2 bg-[#ededed] dark:bg-[#0d1421] ${isScrolled ? 'border-b-2 border-blue-500' : ''}`}
+    >
       <Logo />
       <div className="hidden sm:flex sm:gap-4">
         <LanguageDropdown />

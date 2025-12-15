385fab7c9b9c093038a34ca69303b5eb5fd7a7b5
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-12-15 23:46:38 +0300

    feat(coin-list): add all and favorite coins tabs

--- src/components/CoinList/index.tsx
+++ src/components/CoinList/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, type FC } from 'react';
+import { useMemo, useState, type FC } from 'react';
 import ListHeader from './ListHeader';
 import CoinCard from './CoinCard';
 import { useQuery } from '@tanstack/react-query';
@@ -9,10 +9,16 @@ import NotFound from './NotFound';
 import ListLabel from './ListLabel';
 import CoinCardSkeleton from './CoinCardSkeleton';
 import Error from '../common/Error';
+import { useFavoriteCoinsStore } from '../../store/useFavoriteCoinsStore';
 
 const CoinList: FC = () => {
+  const [activeTab, setActiveTab] = useState<'all' | 'favorites'>('all');
   const containerClasses =
     'bg-[#c2ceec] dark:bg-[#152b55] border-blue-500 border-2 rounded-xl w-full lg:max-w-[25%] lg:min-w-[25%] h-190 2xl:h-[85vh] overflow-y-auto';
+
+  const favoriteCoins = useFavoriteCoinsStore((state) => state.favoriteCoins);
+  const hasFavorites = favoriteCoins.length > 0;
+  const showEmptyFavorites = activeTab === 'favorites' && !hasFavorites;
 
   const filter = useFilterStore((state) => state.filter);
   const {
@@ -29,13 +35,24 @@ const CoinList: FC = () => {
     retry: 3,
   });
 
-  const filteredCoins = useMemo(
+  const textFilteredCoins = useMemo(
     () =>
       coins.filter((coin) =>
         coin.name.toLowerCase().includes(filter.toLowerCase())
       ),
     [coins, filter]
   );
+
+  const filteredCoins = useMemo(() => {
+    if (activeTab === 'favorites') {
+      return textFilteredCoins.filter((coin) =>
+        favoriteCoins.some((fav) => fav.id === coin.id)
+      );
+    }
+
+    return textFilteredCoins;
+  }, [textFilteredCoins, favoriteCoins, activeTab]);
+  const coinsToRender = filteredCoins;
 
   if (isPending || isFetching) {
     return (
@@ -84,13 +101,29 @@ const CoinList: FC = () => {
       <div className="shrink-0">
         <ListHeader />
         <CoinFilter />
-        {filteredCoins.length > 0 && <ListLabel />}
+        <div className="flex items-center gap-5 py-2 px-4 border-b-2 border-b-blue-500">
+          <button
+            className={`text-[20px] font-medium cursor-pointer hover:text-blue-500 ${activeTab === 'all' ? 'text-blue-500 underline underline-offset-2' : ''}`}
+            onClick={() => setActiveTab('all')}
+          >
+            All
+          </button>
+          <button
+            className={`text-[20px] font-medium cursor-pointer hover:text-blue-500 ${activeTab === 'favorites' ? 'text-blue-500 underline underline-offset-2' : ''}`}
+            onClick={() => setActiveTab('favorites')}
+          >
+            Favorites
+          </button>
+        </div>
+        {coinsToRender.length > 0 && <ListLabel />}
       </div>
 
       <div className="flex-1 min-h-0 overflow-y-auto scrollbar-custom">
         <div className="flex flex-col">
-          {filteredCoins.length > 0 ? (
-            filteredCoins.map((coin) => (
+          {showEmptyFavorites ? (
+            <div>List of favorite coins is empty</div>
+          ) : coinsToRender.length > 0 ? (
+            coinsToRender.map((coin) => (
               <CoinCard
                 key={coin.id}
                 id={coin.id}

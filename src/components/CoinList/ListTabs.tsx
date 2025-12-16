f4a656e0de550b83ea8522ec5b56e8a6ea9cddf2
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-12-16 22:03:26 +0300

    fix(list-tabs): add text colors for light and dark theme

--- src/components/CoinList/ListTabs.tsx
+++ src/components/CoinList/ListTabs.tsx
@@ -11,7 +11,7 @@ const ListTabs: FC<ListTabsProps> = ({ activeTab, setActiveTab }) => {
   const language = useLanguageStore((state) => state.language);
 
   return (
-    <div className="flex items-center gap-5 py-2 px-4 border-b-2 border-b-blue-500">
+    <div className="flex items-center gap-5 py-2 px-4 border-b-2 border-b-blue-500 text-black dark:text-gray-300">
       <button
         className={`text-[20px] font-medium cursor-pointer hover:text-blue-500 ${activeTab === 'all' ? 'text-blue-500 underline underline-offset-2' : ''}`}
         onClick={() => setActiveTab('all')}

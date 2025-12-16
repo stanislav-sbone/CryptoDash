253231c0395488e2b482f3bc54c077997407c595
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-12-16 21:10:45 +0300

    feat(coin-card): add button for adding or removing coin from favorite list

--- src/components/CoinList/ListLabel.tsx
+++ src/components/CoinList/ListLabel.tsx
@@ -5,10 +5,11 @@ import { translations } from '../../locales/translations';
 const ListLabel: FC = () => {
   const language = useLanguageStore((state) => state.language);
   return (
-    <div className="grid grid-cols-[60px_1fr_auto] font-medium text-black dark:text-gray-300 mt-1 px-6">
+    <div className="grid grid-cols-[60px_1fr_auto_50px] font-medium text-black dark:text-gray-300 mt-1 px-6">
       <p></p>
       <p>{translations[language].name}</p>
       <p className="justify-self-end">{translations[language].change24h}</p>
+      <p></p>
     </div>
   );
 };

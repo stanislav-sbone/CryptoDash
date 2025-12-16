a6706066ec0db72afe856d95bf5d25d8ec186b59
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-12-16 21:58:08 +0300

    feat(coin-card): add styles for active coin and update styles of hover effect

--- src/components/CoinList/CoinCard.tsx
+++ src/components/CoinList/CoinCard.tsx
@@ -12,6 +12,7 @@ const CoinCard: FC<ICoin> = ({
   current_price,
   price_change_24h,
 }) => {
+  const currentCoin = useCoinStore((state) => state.coinID);
   const setCoin = useCoinStore((state) => state.setCoinID);
   const favoriteCoins = useFavoriteCoinsStore((state) => state.favoriteCoins);
   const addFavorite = useFavoriteCoinsStore((state) => state.addFavorite);
@@ -40,31 +41,37 @@ const CoinCard: FC<ICoin> = ({
 
   return (
     <div
-      className="group grid grid-cols-[50px_1fr_auto_50px] md:grid-cols-[70px_1fr_auto_50px] items-center font-medium px-4 py-3 cursor-pointer transition-colors duration-300 ease-in-out hover:bg-blue-500 dark:hover:bg-[#27509c]"
+      className={`group grid grid-cols-[50px_1fr_auto_50px] md:grid-cols-[70px_1fr_auto_50px] items-center font-medium px-4 py-3 cursor-pointer transition-colors duration-300 ease-in-out ${currentCoin === id ? 'bg-blue-500' : 'hover:bg-blue-500 dark:hover:bg-[#27509c]'}`}
       onClick={() => setCoin(id)}
     >
       <img src={image} alt={symbol} className="w-8 h-8 md:w-12 md:h-12" />
       <div>
-        <h3 className="text-[14px] md:text-[18px] tracking-wider text-black dark:text-white">
+        <h3
+          className={`text-[14px] md:text-[18px] tracking-wider ${currentCoin === id ? 'text-white' : 'text-black dark:text-white'} group-hover:text-white`}
+        >
           <span>{name}</span>
-          <span className="text-gray-700 dark:text-gray-400 tracking-tight ml-2">
+          <span
+            className={`${currentCoin === id ? 'text-white' : 'text-gray-700 dark:text-gray-400'} group-hover:text-white tracking-tight ml-2`}
+          >
             {symbol.toUpperCase()}
           </span>
         </h3>
-        <p className="text-[16px] font-medium tracking-wider text-black dark:text-white">
+        <p
+          className={`text-[16px] font-medium tracking-wider ${currentCoin === id ? 'text-white' : 'text-black dark:text-white'} group-hover:text-white`}
+        >
           ${formatNumber(current_price.toFixed(2))}
         </p>
       </div>
       <div className="ml-auto">
         <span
-          className={price_change_24h > 0 ? 'text-green-600' : 'text-red-600'}
+          className={price_change_24h > 0 ? 'text-green-400' : 'text-red-600'}
         >
           {price_change_24h > 0 && '+'}
           {formatPriceChange(price_change_24h)}$
         </span>
       </div>
       <button
-        className={`${isFavorite ? 'lg:opacity-100' : 'lg:opacity-0'} cursor-pointer p-1 transition-all duration-200 group-hover:scale-120 group-hover:opacity-100 justify-self-end`}
+        className={`${isFavorite ? 'lg:opacity-100' : 'lg:opacity-0'} cursor-pointer p-1 transition-all duration-400 group-hover:scale-120 group-hover:opacity-100 justify-self-end`}
         onClick={(event) => handleFavoriteClick(event)}
       >
         <svg
@@ -72,11 +79,11 @@ const CoinCard: FC<ICoin> = ({
           height="20"
           viewBox="0 0 24 24"
           fill={isFavorite ? '#fbbf24' : 'none'}
-          stroke={isFavorite ? '#fbbf24' : 'currentColor'}
+          stroke="#fbbf24"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="text-gray-700 dark:text-gray-300 transition-colors duration-200"
+          className="text-gray-700 dark:text-gray-300 md: transition-colors duration-200 group-hover:fill-[#fbbf24] group-hover:stroke-[#fbbf24]"
         >
           <path d="M11.2691 4.41115C11.5006 3.89177 11.6164 3.63208 11.7776 3.55211C11.9176 3.48263 12.082 3.48263 12.222 3.55211C12.3832 3.63208 12.499 3.89177 12.7305 4.41115L14.5745 8.54808C14.643 8.70162 14.6772 8.77839 14.7302 8.83718C14.777 8.8892 14.8343 8.93081 14.8982 8.95929C14.9705 8.99149 15.0541 9.00031 15.2213 9.01795L19.7256 9.49336C20.2911 9.55304 20.5738 9.58288 20.6997 9.71147C20.809 9.82316 20.8598 9.97956 20.837 10.1342C20.8108 10.3122 20.5996 10.5025 20.1772 10.8832L16.8125 13.9154C16.6877 14.0279 16.6252 14.0842 16.5857 14.1527C16.5507 14.2134 16.5288 14.2807 16.5215 14.3503C16.5132 14.429 16.5306 14.5112 16.5655 14.6757L17.5053 19.1064C17.6233 19.6627 17.6823 19.9408 17.5989 20.1002C17.5264 20.2388 17.3934 20.3354 17.2393 20.3615C17.0619 20.3915 16.8156 20.2495 16.323 19.9654L12.3995 17.7024C12.2539 17.6184 12.1811 17.5765 12.1037 17.56C12.0352 17.5455 11.9644 17.5455 11.8959 17.56C11.8185 17.5765 11.7457 17.6184 11.6001 17.7024L7.67662 19.9654C7.18404 20.2495 6.93775 20.3915 6.76034 20.3615C6.60623 20.3354 6.47319 20.2388 6.40075 20.1002C6.31736 19.9408 6.37635 19.6627 6.49434 19.1064L7.4341 14.6757C7.46898 14.5112 7.48642 14.429 7.47814 14.3503C7.47081 14.2807 7.44894 14.2134 7.41394 14.1527C7.37439 14.0842 7.31195 14.0279 7.18708 13.9154L3.82246 10.8832C3.40005 10.5025 3.18884 10.3122 3.16258 10.1342C3.13978 9.97956 3.19059 9.82316 3.29993 9.71147C3.42581 9.58288 3.70856 9.55304 4.27406 9.49336L8.77835 9.01795C8.94553 9.00031 9.02911 8.99149 9.10139 8.95929C9.16534 8.93081 9.2226 8.8892 9.26946 8.83718C9.32241 8.77839 9.35663 8.70162 9.42508 8.54808L11.2691 4.41115Z" />
         </svg>
